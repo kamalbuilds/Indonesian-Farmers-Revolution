0ac039bace1043fdf58e214192de49d08a38c2cc
Kamal Nayan <geniusamansingh@gmail.com>
2024-07-23 22:05:26 +0530

    contracts init

---
+++ contracts/README.md
@@ -0,0 +1,7 @@
+## Commands
+
+- Install dependencies - `npm install`
+- Clean project - `npx hardhat clean`
+- Compile contracts and generate typechain types - `npx hardhat compile`
+- Run tests - `npx hardhat test`
+- Run local node - `npx hardhat node`
---
+++ frontend/config/site.ts
@@ -0,0 +1,33 @@
+import { Chain, polygon, sepolia, telos } from "viem/chains";
+
+export type SiteConfig = typeof siteConfig;
+
+export type SiteConfigContracts = {
+  chain: Chain;
+  farmersToken: `0x${string}`;
+  usdcToken: `0x${string}`;
+  entryPoint: `0x${string}`;
+  paymaster: `0x${string}`;
+  accountFactory: `0x${string}`;
+  accountAbstractionSuported: boolean;
+};
+
+export const siteConfig = {
+  emoji: "🧑‍🌾",
+  name: "Indonesia Farmers Revolution",
+  description: "Dapp that connects farmers with the right investors through the tokenization of their real-world assets (RWAs) like crops and farm products",
+  links: {
+    github: "https://github.com/kamalbuilds/Indonesian-Farmers-Onchain",
+  },
+  contracts: [
+    {
+      // chain: { id: 56, name: "Binance Smart Chain" },
+      chain: sepolia,
+    },
+    {
+      // chain: { id: 137, name: "Polygon" },
+      chain: polygon,
+    }
+  ]
+};
+
