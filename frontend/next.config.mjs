9603db64858214b064fdb95a8b5ae1ed26bcdba3
Kamal Nayan <geniusamansingh@gmail.com>
2024-07-23 23:05:26 +0530

    shadcn init

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false }; // Fix for Pinata storage
+    return config;
+  },
+};
 
 export default nextConfig;
